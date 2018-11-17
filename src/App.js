76e7696b57856267be58858e3e7468f418ffbe5e
devonChurch <notifydevon@gmail.com>
2018-11-17 23:40:42 +1300

    feat: 🎸 add x and y axis to bar chart

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React, { Component } from "react";
 import createRandom from "lodash.random";
 import BarChart from "./chart/bar/";
 
-const createEntry = name => ({ name, total: createRandom(1, 10) });
-const createData = () =>
-  ["potato", "apple", "banana", "pumpkin", "orange"].map(createEntry);
+const createEntry = (_, index) => ({
+  date: new Date(`2018-11-${index + 1}`),
+  total: createRandom(1, 10)
+});
+const createData = () => new Array(10).fill(0).map(createEntry);
 
 class App extends Component {
   state = { data: createData() };
 
   render() {
     const { data } = this.state;
-    const shell = { x: [], y: [] };
-    const { x: xData, y: yData } = data.reduce(
-      ({ x, y }, { name, total }) => ({
-        x: [...x, name],
-        y: [...y, total]
-      }),
-      shell
-    );
+    // const shell = { x: [], y: [] };
+    // const { x: xData, y: yData } = data.reduce(
+    //   ({ x, y }, { date, total }) => ({
+    //     x: [...x, date],
+    //     y: [...y, total]
+    //   }),
+    //   shell
+    // );
     return (
       <div>
         <BarChart
-          data={this.state.data}
-          xData={xData}
-          yData={yData}
+          data={data}
+          xAxisMargin={20}
+          yAxisMargin={50}
           // From container query....
-          height={300}
-          width={500}
+          svgHeight={300}
+          svgWidth={500}
         />
       </div>
     );
